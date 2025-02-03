797bf0c2481d7c4e875be5aba56a036812950254
Abuzar-asif <Abuzar@pixarsart.com>
2025-02-03 23:52:50 +0500

    animate

--- src/Technology/Technology.jsx
+++ src/Technology/Technology.jsx
@@ -1,23 +1,146 @@
-import React from 'react'
-import '../App.css'
-import '../Technology/Technology.css'
+import React from "react";
+import "../App.css";
+import "../Technology/Technology.css";
 const Technology = () => {
   return (
     <>
       <div className="container-fluid  row  technology d-flex text-white justify-content-start align-items-center flex-wrap py-5">
-            <div className='one'> <p>Webflow</p></div>
-            <div className='one'> <p>Vue.js</p></div>
-            <div className='one'> <p>Angular js</p></div>
-            <div className='one'> <p>Python</p></div>
-            <div className='one'> <p>Laravel</p></div>
-            <div className='two divwidth1'> <p>Wordpress</p></div>
-            <div className='two divwidth2'> <p>Shopify</p></div>
-            <div className='two divwidth3'> <p>WIX partners</p></div>
-            <div className='two divwidth4'> <p>Javascript</p></div>
-            <div className='two divwidth5'> <p>php</p></div>
+        {/* <svg
+          version="1.2"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1920 1080"
+          width="1920"
+          height="1080"
+        >
+          <path
+            stroke="black"
+            id="Shape"
+            fill-rule="evenodd"
+            class="s0"
+            d="m0 545.2v-11.2h1920v11.2z"
+          />
+          <text>
+            <textPath href="#Shape">
+            <div className="one">
+          {" "}
+          <p>Webflow</p>
+        </div>
+        <div className="one">
+          {" "}
+          <p>Vue.js</p>
+        </div>
+        <div className="one">
+          {" "}
+          <p>Angular js</p>
+        </div>
+        <div className="one">
+          {" "}
+          <p>Python</p>
+        </div>
+        <div className="one">
+          {" "}
+          <p>Laravel</p>
+        </div>
+        <div className="two divwidth1">
+          {" "}
+          <p>Wordpress</p>
+        </div>
+        <div className="two divwidth2">
+          {" "}
+          <p>Shopify</p>
+        </div>
+        <div className="two divwidth3">
+          {" "}
+          <p>WIX partners</p>
+        </div>
+        <div className="two divwidth4">
+          {" "}
+          <p>Javascript</p>
+        </div>
+        <div className="two divwidth5">
+          {" "}
+          <p>php</p>
+        </div>
+            </textPath>
+          </text>
+        </svg> */}
+{/* 
+<svg
+  version="1.2"
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 1920 1080"
+  width="1920"
+  height="1080"
+>
+  <path
+    stroke="black"
+    id="Shape"
+    fillRule="evenodd"
+    className="s0"
+    d="m0 545.2v-11.2h1920v11.2z"
+  />
+  <text  id="#Shape"  className="one">Webflow</text>
+  <text  id="#Shape"   className="one">Vue.js</text>
+  <text   id="#Shape"  className="one">Angular js</text>
+  <text   id="#Shape" className="one">Python</text>
+  <text  id="#Shape"  className="one">Laravel</text>
+  <text  id="#Shape"  className="two">Wordpress</text>
+  <text  id="#Shape"  className="two">Shopify</text>
+  <text   id="#Shape" className="two">WIX partners</text>
+  <text   id="#Shape" className="two">Javascript</text>
+  <text   id="#Shape" className="two">php</text>
+</svg> */}
+
+
+
+
+
+
+        
+
+       <div className="one">
+          {" "}
+          <p>Webflow</p>
+        </div>
+        <div className="one">
+          {" "}
+          <p>Vue.js</p>
+        </div>
+        <div className="one">
+          {" "}
+          <p>Angular js</p>
+        </div>
+        <div className="one">
+          {" "}
+          <p>Python</p>
+        </div>
+        <div className="one">
+          {" "}
+          <p>Laravel</p>
+        </div>
+        <div className="two divwidth1">
+          {" "}
+          <p>Wordpress</p>
+        </div>
+        <div className="two divwidth2">
+          {" "}
+          <p>Shopify</p>
+        </div>
+        <div className="two divwidth3">
+          {" "}
+          <p>WIX partners</p>
+        </div>
+        <div className="two divwidth4">
+          {" "}
+          <p>Javascript</p>
+        </div>
+        <div className="two divwidth5">
+          {" "}
+          <p>php</p>
+        </div> 
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Technology
+export default Technology;
