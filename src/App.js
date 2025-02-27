9ae0d6c62f4531824f62a277a45fedd64d73f8fb
ABUBAKAR SAFEER <abubakr@pixarsart.com>
2025-02-27 23:59:43 +0500

    created moving mouse animation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,88 @@
-import React from "react";
+// import React from "react";
+// import "./App.css";
+// import Navhead from "./Navbar/Navbar";
+// import Backgroundimage from "./BackgroundImage/Backgroundimage";
+// import Partners from "./Partners/Partners";
+// import Technology from "./Technology/Technology";
+// import Offer from "./Offer/Offer";
+// import Step from "./Step/Step";
+// // import Brand from './Brands/Brand'
+// import Client from "./Client/Client";
+// import ChooseUs from "./ChooseUs/ChooseUs";
+// import Hearmore from "./Hearmore/Hearmore";
+// import GetaQuote from "./GetaQuote/GetaQuote";
+// import Gallery from "./Gallery/Gallery";
+// import Footer from "./Footer/Footer";
+// // import Cards from './Cards/Cards'
+// import Service from "./Service/Service";
+// import Contactfooter from "./Contactfooter/Contactfooter";
+// import WBrand from "./Wbrand/WBrand";
+// import Rcard from "./RCard/Rcard";
+// // import MarqueAnimation from './Gsapcode'
+// // import { Routes, Route } from "react-router-dom";
+// import newRoute from "./Routes/Route";
+
+// import ModalFullscreenExample from "./Modals/Modal1";
+// import GetinTouch from "./GetinTouch/GetinTouch";
+// // import { createPortal } from 'react-dom'
+// // const mountElement=document.getElementById('overlays')
+
+// const App = () => {
+//   return (
+//     <>
+//       {/* <ModalFullscreenExample /> */}
+//       {/* {
+//       createPortal(
+//         <ModalFullscreenExample/>,mountElement
+
+//       )
+//     } */}
+//       {/* <Routes>{newRoute.map((currentvalue,index)=>(
+//   <Route path={currentvalue.path} 
+//   element={currentvalue.component}
+//   key={index}
+//   />
+
+
+//   ))} */}
+//       {/* <Routes>
+//   <Route path='/Service' Component={Service}/>
+//  </Routes> */}
+//       {/* <GetinTouch /> */}
+// <div className="cursor-dot" data-cursor-dot></div>
+//       <div className="cursor-outline" data-cursor-dot></div>
+//       <Navhead />
+//       <Backgroundimage />
+//       <Partners />
+//       <Offer />
+//       <Technology />
+//       <Service />
+//       <Step />
+//       {/* <Brand/> */}
+//       <WBrand />
+//       <Gallery />
+//       <ChooseUs />
+//       <Hearmore />
+//       <Client />
+//       {/* <Cards/> */}
+//       <Rcard />
+//       <GetaQuote />
+//       {/* <MarqueAnimation/>  */}
+//       <Footer />
+//       <Contactfooter />
+
+//       {/* </Routes> */}
+//     </>
+//   );
+// };
+
+// export default App;
+
+
+
+
+
+import React, { useEffect } from "react";
 import "./App.css";
 import Navhead from "./Navbar/Navbar";
 import Backgroundimage from "./BackgroundImage/Backgroundimage";
@@ -6,49 +90,53 @@ import Partners from "./Partners/Partners";
 import Technology from "./Technology/Technology";
 import Offer from "./Offer/Offer";
 import Step from "./Step/Step";
-// import Brand from './Brands/Brand'
 import Client from "./Client/Client";
 import ChooseUs from "./ChooseUs/ChooseUs";
 import Hearmore from "./Hearmore/Hearmore";
 import GetaQuote from "./GetaQuote/GetaQuote";
 import Gallery from "./Gallery/Gallery";
 import Footer from "./Footer/Footer";
-// import Cards from './Cards/Cards'
-import Service from "./Service/Service";
-import Contactfooter from "./Contactfooter/Contactfooter";
 import WBrand from "./Wbrand/WBrand";
 import Rcard from "./RCard/Rcard";
-// import MarqueAnimation from './Gsapcode'
-// import { Routes, Route } from "react-router-dom";
-import newRoute from "./Routes/Route";
-
+import Service from "./Service/Service";
+import Contactfooter from "./Contactfooter/Contactfooter";
 import ModalFullscreenExample from "./Modals/Modal1";
 import GetinTouch from "./GetinTouch/GetinTouch";
-// import { createPortal } from 'react-dom'
-// const mountElement=document.getElementById('overlays')
 
 const App = () => {
+  useEffect(() => {
+    const cursorDot = document.querySelector("[data-cursor-dot]");
+    const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+    const mouseMoveHandler = (e) => {
+      const posX = e.clientX;
+      const posY = e.clientY;
+
+      cursorDot.style.left = `${posX}px`;
+      cursorDot.style.top = `${posY}px`;
+      cursorOutline.animate(
+        {
+          left: `${posX}px`,
+          top: `${posY}px`,
+        },
+        {
+          duration: 500,
+          fill: "forwards",
+        }
+      );
+    };
+
+    window.addEventListener("mousemove", mouseMoveHandler);
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMoveHandler);
+    };
+  }, []);
+
   return (
     <>
-      {/* <ModalFullscreenExample /> */}
-      {/* {
-      createPortal(
-        <ModalFullscreenExample/>,mountElement
-
-      )
-    } */}
-      {/* <Routes>{newRoute.map((currentvalue,index)=>(
-  <Route path={currentvalue.path} 
-  element={currentvalue.component}
-  key={index}
-  />
-
-
-  ))} */}
-      {/* <Routes>
-  <Route path='/Service' Component={Service}/>
- </Routes> */}
-      <GetinTouch />
+      <div className="cursor-dot" data-cursor-dot></div>
+      <div className="cursor-outline" data-cursor-outline></div>
       <Navhead />
       <Backgroundimage />
       <Partners />
@@ -56,22 +144,19 @@ const App = () => {
       <Technology />
       <Service />
       <Step />
-      {/* <Brand/> */}
       <WBrand />
       <Gallery />
       <ChooseUs />
       <Hearmore />
       <Client />
-      {/* <Cards/> */}
       <Rcard />
       <GetaQuote />
-      {/* <MarqueAnimation/>  */}
       <Footer />
       <Contactfooter />
-
-      {/* </Routes> */}
     </>
   );
 };
 
 export default App;
+
+
